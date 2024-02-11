6247c612136caa676dafc2893c638ddbb350993d
MVN home <wiep1994@yandex.ru>
2024-02-11 19:14:13 +0300

    add application

---
+++ bootconfigdemo/src/main/java/com/example/bootconfigdemo/BootconfigdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootconfigdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootconfigdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootconfigdemoApplication.class, args);
+	}
+
+}
---
+++ bootconfigdemo/src/test/java/com/example/bootconfigdemo/BootconfigdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootconfigdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootconfigdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
